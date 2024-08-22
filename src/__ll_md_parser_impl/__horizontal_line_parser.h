a5f46aae93b6566bc15971a90d25f0aeb93d310b
Elkeid Me <elkeid.me@gmail.com>
2024-08-22 23:30:28 +0800

    Add GPLv3 comments.

--- src/__ll_md_parser_impl/__horizontal_line_parser.h
+++ src/__ll_md_parser_impl/__horizontal_line_parser.h
@@ -1,3 +1,20 @@
+// Copyright (C) 2022-2024 Elkeid-me
+//
+// This file is part of HomoDark.
+//
+// HomoDark is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// HomoDark is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with HomoDark.  If not, see <http://www.gnu.org/licenses/>.
+
 #if __cplusplus >= 201703L
 #ifndef __HORIZONTAL_LINE_PARSER_H
 #define __HORIZONTAL_LINE_PARSER_H 1
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,21 @@
-#include "main_window.h"
+// Copyright (C) 2022-2024 Elkeid-me
+//
+// This file is part of HomoDark.
+//
+// HomoDark is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// HomoDark is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with HomoDark.  If not, see <http://www.gnu.org/licenses/>.
 
+#include "main_window.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
